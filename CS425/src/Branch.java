f5416a50d2c0637099fc0f65a3f2fa68a31623cf
namdarine <96828702+namdarine@users.noreply.github.com>
2022-11-29 16:07:02 -0600

    Add files via upload

---
+++ CS425/src/Branch.java
@@ -0,0 +1,13 @@
+public class Branch<T> {
+    private T address;
+
+    // Setter
+    public void setAddress (T address) {
+        this.address = address;
+    }
+
+    // Getter
+    public T getAddress() {
+        return address;
+    }
+}
