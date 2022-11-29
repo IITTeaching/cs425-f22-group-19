07ca56660c0fa71ec374d3cf2ab15c5f843ce124
Chirashe Dziire <chirashed@gmail.com>
2022-11-29 17:22:16 -0600

    Template Start

--- CS425/src/Main.java
+++ CS425/src/Main.java
@@ -1,5 +1,124 @@
-public class Main {
-    public static void main(String[] args) throws Exception {
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Connection;
 
+public class Main {
+
+	public static final String JDBC_DRIVER = "org.postgresql.Driver";
+	// postgres URLs are of the form: jdbc:postgresql://host:port/database
+	public static final String JDBC_DB = "bank";
+	public static final String JDBC_PORT = "5432";
+	public static final String JDBC_HOST = "127.0.0.1";
+	public static final String JDBC_URL = "jdbc:postgresql://" + JDBC_HOST + ":" + JDBC_PORT + "/" + JDBC_DB;
+	public static final String DBUSER = "postgres";
+	public static final String DBPASSWD = "abc123";
+    public static Connection c;
+
+	public static void main (String[] args) throws Exception {
+		try {
+			// load the driver based on the drivers class name
+			Class.forName(JDBC_DRIVER);
+			// create a connection
+			c = DriverManager.getConnection(JDBC_URL, DBUSER, DBPASSWD);
+            
+            System.out.println("Connection to Bank Database Successful.");	
+            
+            login();
+            // PreparedStatement pStmt = c.prepareStatement("INSERT INTO branch (add1, add2, city, state, zip) VALUES(?,?,?,?,?)");
+			
+			// pStmt.setString(1, "Test Branch Add1");
+			// pStmt.setString(2, "Test Branch Add2");
+			// pStmt.setString(3, "Chicago");
+			// pStmt.setString(4, "IL");
+            // pStmt.setInt(5, 60305);
+			// pStmt.executeUpdate();
+
+		}
+		catch (Exception e) {
+			System.err.println("An error occurred: " + e.toString());
+			System.err.println("\n\nFOR THIS PROGRAM TO WORK YOU HAVE TO HAVE A POSTGRES SERVER RUNNING LOCALLY (OR DOCKER) AT "
+							   + JDBC_HOST
+							   + " WITH PORT " + JDBC_PORT
+							   + " AND DATABASE " + JDBC_DB
+							   + " AND USER " + DBUSER
+							   + " WITH PASSWORD " + DBPASSWD);
+		}
+	}
+	
+    public static void login() throws Exception{
+            char user_type;
+            while(true){
+            System.out.println("Select Login Role:");	
+            System.out.println("Enter 1: Manager");	
+            System.out.println("Enter 2: Loan Manager");
+            System.out.println("Enter 3: Teller");
+            System.out.println("Enter 4: Customer");
+            System.out.println("Enter 5: Exit");
+            user_type = ((char)System.in.read());
+            switch((int)user_type-48){
+                case 1:{
+                    System.out.println("Logged in as Manager.");
+                    break;
+                }
+                case 2:{
+                    System.out.println("Logged in as Loan Manager.");
+                    break;
+                }
+                case 3:{
+                    System.out.println("Logged in as Teller.");
+                    break;
+                }
+                case 4:{
+                    System.out.println("Logged in as Customer.");
+                    User_Customer.start();
+                    break;
+                }
+                case 5:{
+                    System.out.println("Exited.");
+                    return;
+                }
+                default:{
+                    System.out.println("Invalid role selected, please try again.");
+                    break;
+                }   
+            }
+        }
     }
+
+    	// method for printing results for any query
+	public static void printQueryResults(Connection c, String sql) {
+		try {
+			Statement s = c.createStatement();
+			ResultSet r = s.executeQuery(sql);
+			ResultSetMetaData md = r.getMetaData();
+			int numCols = md.getColumnCount();
+
+			System.out.printf("================================================================================" +
+							  "\nQUERY: %s" +
+							  "\n================================================================================\n",
+							  sql);
+			while(r.next()) {
+				System.out.print("(");
+				for(int i = 1; i <= numCols; i++) {
+					System.out.printf("%s: %s%s",
+									  md.getColumnName(i),
+									  r.getString(i),
+									  i < numCols ? ", ": ""
+						);					
+				}
+				System.out.println(")");					
+			}
+			
+			r.close();
+			s.close();
+		}
+		catch (SQLException e) {
+			System.err.println("An error occurred: " + e.toString());			
+		}
+	}
+	
 }
---
+++ CS425/src/User_Customer.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+public class User_Customer {
+    public static void start() throws Exception{
+        boolean loggedIn = true;
+        while(loggedIn){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Select Option:");	
+            System.out.println("Enter 1: Create/Remove Account");	
+            System.out.println("Enter 2: Withdrawl");
+            System.out.println("Enter 3: Deposit");
+            System.out.println("Enter 4: Check Balance");
+            System.out.println("Enter 5: Logout");
+            int action = scan.nextInt();
+            switch(action){
+                    case 1:{
+                        
+                        break;
+                    }
+                    case 2:{
+                        
+                        break;
+                    }
+                    case 3:{
+                        
+                        break;
+                    }
+                    case 4:{
+                        
+                        break;
+                    }
+                    case 5:{
+                        System.out.println("Logged out.");
+                        loggedIn = false;
+                        break;
+                    }
+                    default:{
+                        System.out.println("Invalid option selected, please try again.");
+                        break;
+                   }   
+            }
+        }
+    }
+}
