3a90db9a56c6c5e4953f5f88314ac895beb758fd
namdarine <namdarine.code@gmail.com>
2022-11-29 20:29:24 -0600

    add 'create' function in 'Account class'

--- CS425/src/Account.java
+++ CS425/src/Account.java
@@ -1,11 +1,16 @@
+import java.util.*;
+
 public class Account<T> {
-    private T accountID;
+    private T accNum;
+    private T accPin;
     private T balance;
     private T type;
 
+    private static final Map<Integer, Account> accounts = new HashMap<>();
     // Setter
-    public void setAccountID(T accountID) {
-        this.accountID = accountID;
+    public Account(T accNum, T accPin) {
+        this.accNum = accNum;
+        this.accPin = accPin;
     }
 
     public void setBalance(T balance) {
@@ -17,9 +22,11 @@ public class Account<T> {
     }
 
     // Getter
-    public T getAccountID() {
-        return accountID;
+    public T getAccNum() {
+        return accNum;
     }
+
+    public T getAccPin() { return accPin;}
 
     public T getBalance() {
         return balance;
@@ -27,5 +34,15 @@ public class Account<T> {
 
     public T getType() {
         return type;
+    }
+
+    public static void create() {
+        System.out.println("Create six digit account number: ");
+        Scanner input = new Scanner(System.in);
+        int accNum = input.nextInt();
+        System.out.println("Create four digit account pin: ");
+        Scanner input2 = new Scanner(System.in);
+        int accPin = input2.nextInt();
+        Account account = new Account(accNum, accPin);
     }
 }
