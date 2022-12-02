f967815387d1602c9a4ad5e8733dbe9d287727f1
Chirashe Dziire <chirashed@gmail.com>
2022-12-01 18:38:50 -0600

    Logging into customer accounts

--- CS425/src/Customer.java
+++ CS425/src/Customer.java
@@ -1,3 +1,9 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.lang.*;
+
 public class Customer<T> {
     private T SSN;
     private T name;
@@ -36,5 +42,26 @@ public class Customer<T> {
 
     public T getHomeBranch() {
         return homeBranch;
+    }
+
+    public static boolean exists(int SSN) throws Exception
+    {
+        try
+        {
+            PreparedStatement pStmt = Main.c.prepareStatement("SELECT* from customer WHERE ssn = ?;");
+            pStmt.setInt(1, SSN);
+            pStmt.executeQuery();
+            ResultSet r = pStmt.executeQuery();
+            int count = 0;
+            while(r.next()){
+                count++;
+			}
+			r.close();
+            return count > 0;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
     }
 }
--- CS425/src/User_Customer.java
+++ CS425/src/User_Customer.java
@@ -2,19 +2,33 @@
 import java.util.Scanner;
 public class User_Customer {
     public static void start() throws Exception{
-        boolean loggedIn = true;
-        while(loggedIn){
-            Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        boolean loggedIn = false;
+        int SSN = 0;
+
+        while(!loggedIn){
+            System.out.println("Enter SSN or type -1 to cancel: ");
+            SSN = scan.nextInt();
+            if(SSN > 99999999 && SSN < 1000000000){
+                if(Customer.exists(SSN))
+                    loggedIn = true;
+            }
+            else if(SSN == -1)
+                return;
+        }
+
+        while(loggedIn){ 
             System.out.println("Select Option:");	
-            System.out.println("Enter 1: Create/Remove Account");	
-            System.out.println("Enter 2: Withdrawl");
-            System.out.println("Enter 3: Deposit");
-            System.out.println("Enter 4: Check Balance");
-            System.out.println("Enter 5: Logout");
+            System.out.println("Enter 1: Create Account");	
+            System.out.println("Enter 2: Remove Account");
+            System.out.println("Enter 3: Withdrawl");
+            System.out.println("Enter 4: Deposit");
+            System.out.println("Enter 5: Check Balance");
+            System.out.println("Enter 6: Logout");
             int action = scan.nextInt();
             switch(action){
                     case 1:{
-                        Account.create();
+                        Account.create(SSN);
                         break;
                     }
                     case 2:{
@@ -30,6 +44,10 @@ public class User_Customer {
                         break;
                     }
                     case 5:{
+                        
+                        break;
+                    }
+                    case 7:{
                         System.out.println("Logged out.");
                         loggedIn = false;
                         break;
